c57ea1edfdeec23a5bbf5cac030a9cee420d6634
ParkerH27 <91337218+ParkerH27@users.noreply.github.com>
2023-04-28 00:23:54 +0000

    Initial commit

---
+++ levelmake.py
@@ -0,0 +1,37 @@
+# connected component labeling
+#img to level
+from PIL import Image
+
+def image_to_binary(image_path):
+    # Open the image file
+    image = Image.open(image_path)
+
+    # Convert the image to black and white
+    image = image.convert("1")
+
+    # Get the pixel data as a list of tuples
+    pixel_data = list(image.getdata())
+
+    # Get the image size
+    width, height = image.size
+
+    # Convert the pixel data to a 2D array of 1s and 0s
+    binary_data = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            pixel = pixel_data[y * width + x]
+            row.append(int(pixel == 0))
+        binary_data.append(row)
+
+    # Write the binary data to a file
+    with open("level1.txt", "w") as f:
+        for row in binary_data:
+            for pixel in row:
+                f.write("╋" if pixel == 1 else " ")
+            f.write("\n")
+
+    # Return the binary data as a 2D array
+    return binary_data
+
+print(image_to_binary("img.jpg"))
