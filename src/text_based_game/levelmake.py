26c000029f74acd1492c1111627547d88d1863cb
Eli <88557639+lishaduck@users.noreply.github.com>
2023-10-26 19:23:46 -0500

    refactor: use an argument parser in levelmake

--- src/text_based_game/levelmake.py
+++ src/text_based_game/levelmake.py
@@ -3,9 +3,13 @@
 
 import logging
 import os
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
 from PIL import Image
+from rich.logging import RichHandler
+
+log = logging.getLogger("level_make")
 
 
 def image_to_binary(image_path: Path, location: Path) -> list[list[bool]]:
@@ -42,9 +46,51 @@ def image_to_binary(image_path: Path, location: Path) -> list[list[bool]]:
     return binary_data
 
 
-p = Path(os.path.realpath(__file__)).parent
-lvl2_file = p / "level2.txt"
-img = p / "img.jpg"
+def build_parser() -> Namespace:
+    """Build up the argument parser."""
+    parser = ArgumentParser(description="Convert an image to a level.")
 
-image_to_binary(img, lvl2_file)
-logging.info("Finished!")
+    parser.add_argument(
+        "--input",
+        "-i",
+        help="The image to convert.",
+        type=str,
+        default="img.jpg",
+    )
+
+    parser.add_argument(
+        "--output",
+        "-o",
+        help="The file to write the level to.",
+        type=str,
+        default="level2.txt",
+    )
+
+    return parser.parse_args()
+
+
+def run() -> None:
+    """Run the generator."""
+    parser_args: Namespace = build_parser()
+
+    p = Path(os.path.realpath(__file__)).parent
+
+    img = p / parser_args.input
+    level_file = p / parser_args.output
+
+    image_to_binary(img, level_file)
+    log.info("Finished!")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level="INFO",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[
+            RichHandler(
+                rich_tracebacks=True,
+            ),
+        ],
+    )
+    run()
